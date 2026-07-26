b5edeedd7f4ffe086bee258a8933045746175ac0
marcoepunkt <m.endrijat@googlemail.com>
2026-07-26 04:45:31 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "fokusyou-v01";
+const FILES = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
